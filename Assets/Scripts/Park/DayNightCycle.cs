0cf17ce6ecb83dd451406b8700e2ac277114ca77
Heather <35993724+HeatherCochrane@users.noreply.github.com>
2020-06-05 23:51:33 +0100

    Day/night cycle
    Added in light changes for day and night cycle

---
+++ Assets/Scripts/Park/DayNightCycle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+	[SerializeField]
+	Light mainLight;
+	//https://gamedev.stackexchange.com/questions/118305/how-do-i-lerp-text-color-over-time
+	IEnumerator UpdateLightColor(Color32 start, Color32 end)
+	{
+		float t = 0;
+		while (t < 1)
+		{
+			// Now the loop will execute on every end of frame until the condition is true
+			mainLight.color = Color.Lerp(start, end, t);
+			t += Time.deltaTime / 2;
+			yield return new WaitForEndOfFrame(); // So that I return something at least.
+		}
+	}
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void dayCycle()
+	{
+		StartCoroutine(UpdateLightColor(new Color32(56, 24, 77, 1), new Color32(255, 255, 255, 1)));
+	}
+
+	public void nightCycle()
+	{
+		StartCoroutine(UpdateLightColor(new Color32(255, 255, 255, 1), new Color32(56, 24, 77, 1)));	
+	}
+
+	public void nextDay()
+	{
+		StartCoroutine(UpdateLightColor(new Color32(255, 255, 255, 1), new Color32(56, 24, 77, 1)));
+		Invoke("dayCycle", 3);
+	}
+}
