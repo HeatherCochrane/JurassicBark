be7e907a507d9de543edfadeacfeb6a2d787a299
Heather <35993724+HeatherCochrane@users.noreply.github.com>
2020-06-05 12:36:33 +0100

    Shops
    Added basica functionality for shop window, has 3 sections that can be switched between and the shop screen can be open and closed

---
+++ Assets/Scripts/UI/UIHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//USE THIS SCRIPT TO HANDLE SWITCHING BETWEEN SHOP MENUS, MAIN MENU ETC
+public class UIHandler : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> shopScreens = new List<GameObject>();
+
+    [SerializeField]
+    GameObject shopParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < shopScreens.Count; i++)
+        {
+            shopScreens[i].SetActive(false);
+        }
+
+        setShops(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeShopScreen(GameObject screen)
+    {
+        for(int i =0; i < shopScreens.Count;i++)
+        {
+            if(shopScreens[i] == screen)
+            {
+                shopScreens[i].SetActive(true);
+            }
+            else
+            {
+                shopScreens[i].SetActive(false);
+            }
+        }
+    }
+
+    public void setShops(bool set)
+    {
+        shopParent.SetActive(set);
+    }
+}
